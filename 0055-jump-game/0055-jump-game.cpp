767557aafd3f78f68992c789218e13d48eb9ceff
Amrit Raj Shankar <104200137+amritr42@users.noreply.github.com>
2023-08-21 01:37:57 +0530

    Time: 52 ms (76.42%), Space: 48.4 MB (42.48%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+       // int n=sizeof(nums)/sizeof(nums[0]);
+       int n=nums.size();
+        if(n==1)
+        return true;
+        int r=0;
+        for(int i=0;i<n;i++){
+            if(i>r)
+            return false;
+            r=max(r,nums[i]+i);
+        }
+        return true;
+    }
+};
