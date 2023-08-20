6abc6c477d01584585425337f80ff217a65b4d59
Amrit Raj Shankar <104200137+amritr42@users.noreply.github.com>
2023-08-21 02:48:22 +0530

    Time: 12 ms (82.82%), Space: 16.6 MB (64.39%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.cpp
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -3,15 +3,13 @@ public:
     int jump(vector<int>& nums) {
         int p=0,d=0,j=0;
         for(int i=0;i<nums.size()-1;i++){
-            d=max(d,i+nums[i]);
+            d=(d>(i+nums[i])?d:i+nums[i]);
             if(p==i){
                 j++;
                 p=d;
-                if(p==nums.size()-1)
-                return j;
+                
             }
         }
-        return j;
-        
+      return j;  
     }
 };
