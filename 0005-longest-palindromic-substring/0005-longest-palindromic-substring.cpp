09311722ffa5931f54e55b55ca44904dd51fe30b
Amrit Raj Shankar <104200137+amritr42@users.noreply.github.com>
2023-08-21 02:39:25 +0530

    Time: 35 ms (63.43%), Space: 65.5 MB (28.62%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -1,35 +1,35 @@
 class Solution {
 public:
     string longestPalindrome(string s) {
-        int n=s.size();
-        int l,h;
-        int st=0,e=1;
-        for(int i=1;i<n;i++){
+       int n=s.size();
+       int start=0,end=1;
+       int l,h;
+       for(int i=1;i<n;i++){
+           l=i-1;
+           h=i;
+           while(l>=0&&h<n&&s[l]==s[h]){
+               if((h-l+1)>end){
+                   start=l;
+                   end=h-l+1;
+               }
+               l--;
+               h++;
+           }
             l=i-1;
-            h=i;
-            while(l>=0&&h<n&&s[l]==s[h]){
-                if((h-l+1)>e){
-                    st=l;
-                    e=h-l+1;
-                }
-                l--;
-                h++;
-            }
-             l=i-1;
-            h=i+1;
-            while(l>=0&&h<n&&s[l]==s[h]){
-                if((h-l+1)>e){
-                    st=l;
-                    e=h-l+1;
-                }
-                l--;
-                h++;
-            }
-        }
-        string x;
-        for(int i=st;i<=(e+st-1);i++){
-            x=x+s[i];
-        }
-        return x;
+           h=i+1;
+           while(l>=0&&h<n&&s[l]==s[h]){
+               if((h-l+1)>end){
+                   start=l;
+                   end=h-l+1;
+               }
+               l--;
+               h++;
+           }
+       }
+       string x;
+       for(int i=start;i<=(start+end-1);i++){
+           x=x+s[i];
+       }
+       return x;
     }
 };
