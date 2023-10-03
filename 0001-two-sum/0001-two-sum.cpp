e2615e8cce1a383c04b33ac18079fc7b7cec393e
Amrit Raj Shankar <104200137+amritr42@users.noreply.github.com>
2023-10-03 21:57:27 +0530

    Time: 15 ms (54.93%), Space: 11.7 MB (11.46%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -4,9 +4,10 @@ public:
         
        map<int,int> mp;
        //vector<int> c;
+       int x;
        int n=nums.size();
        for(int i=0;i<n;i++){
-           int x=target-nums[i];
+            x=target-nums[i];
            if(mp.find(x)!=mp.end()){
                auto it=mp.find(x);
               // c.push_back(it->second);
